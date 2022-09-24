9b032e3173a0af62031e8295f109cfa18bf57d08
Leonardo Silva <ldss1@outlook.com>
2022-09-24 02:09:03 -0300

    Aula 22/09

---
+++ 1SEM_PI_I/aula_09_22.java
@@ -0,0 +1,14 @@
+/**
+ * aula_09-22
+ */
+public class aula_09_22 {
+
+  public static void main(String[] args) {
+    int soma = 0, num = 0;
+    while (num <= 7) {
+      soma += num;
+      num++;
+    }
+    System.out.println("Resultado " + soma);
+  }
+}
