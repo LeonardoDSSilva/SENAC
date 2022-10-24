ab522860f667b4236d31562a7ada91443321ceb5
Leonardo Silva <ldss1@outlook.com>
2022-10-24 11:16:44 -0300

    version 01

--- 1SEM_AP_I/Lista03_22-10-18/src/ex01/Ex01.java
+++ 1SEM_AP_I/Lista03_22-10-18/src/ex01/Ex01.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Ex01 {
 	public static void main(String[] args) {
 		String operador, encerrar;
-		int codOperador, lugaresFilmeA = 2, lugaresFilmeB = 5, lugaresFilmeC = 3;
+		int codOperador, lugaresFilmeA = 30, lugaresFilmeB = 35, lugaresFilmeC = 55;
 		double caixa = 0;
 
-
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();
+		
 		Scanner iScanner = new Scanner(System.in);
-
+		
 		System.out.println("Informe o seu nome: ");
 		operador = iScanner.nextLine();
 
@@ -19,37 +21,21 @@ public class Ex01 {
 
 		do {
 
+			System.out.print("\033[H\033[2J");  
+			System.out.flush();
+			
 			exibirFilmes();
-	
 			int filme = iScanner.nextInt();
-	
+			
 			switch (filme) {
 				case 1:
-					if (lugaresFilmeA > 0) {
-						System.out.println("Filme A");
-						lugaresFilmeA--;
-						caixa += 10;
-					} else {
-						System.out.println("Filme A lotado");
-					}
+					caixa += filmeA(lugaresFilmeA--);
 				break;
 				case 2:
-					if (lugaresFilmeB > 0) {
-						System.out.println("Filme B");
-						lugaresFilmeB--;
-						caixa += 15;
-					} else {
-						System.out.println("Filme B lotado");
-					}
+					caixa += filmeB(lugaresFilmeB--);
 				break;
 				case 3:
-					if (lugaresFilmeC > 0) {
-						System.out.println("Filme C");
-						lugaresFilmeC--;
-						caixa += 20;
-					} else {
-						System.out.println("Filme C lotado");
-					}
+					caixa += filmeC(lugaresFilmeC--);
 				break;
 				default: System.out.println("Opção inválida!");
 			}
@@ -57,37 +43,53 @@ public class Ex01 {
 			System.out.println("Você deseja Continuar? (S/N)");
 			encerrar = iScanner.next().toUpperCase();
 		} while (!(encerrar.equalsIgnoreCase("S")));
+		
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();
 
-		System.out.println("Obrigado por utilizar o nosso sistema!");
 		System.out.println("O caixa do cinema é de: " + caixa);
-		System.out.println("O operador " + operador + " com o código " + codOperador + " encerrou o sistema.");
-
+		System.out.println("O operador " + operador + " - cod. " + codOperador + " encerrou o sistema.");
 	}
 
 	private static void exibirFilmes() {
 		System.out.println("Qual o filme que você deseja assistir? ");
-		System.out.println("1 - Filme A");
-		System.out.println("2 - Filme B");
-		System.out.println("3 - Filme C");
+		System.out.println("1 - Sala A: Filme 1");
+		System.out.println("2 - Sala B: Filme 2");
+		System.out.println("3 - Sala c: Filme 3");
 	}	
 
 	private static double filmeA(int lugaresFilmeA) {
-		return 0;
-	}
-
-	private static double filmeC(int lugaresFilmeC) {
-		return 0;
+		if (lugaresFilmeA > 0) {
+			System.out.println("Filme A");
+			return 10;
+		} else {
+			System.out.println("Filme A");
+			System.out.println("Lugares esgotados!");
+			return 0;
+		}
 	}
 
 	private static double filmeB(int lugaresFilmeB) {
-			if (lugaresFilmeB > 0) {
-				System.out.println("Filme B");
-				lugaresFilmeB--;
-				return 10;
-			} else {
-				System.out.println("Filme B");
-				System.out.println("Lugares esgotados!");
-				return 0;
-			}
+		if (lugaresFilmeB > 0) {
+			System.out.println("Filme B");
+			return 10;
+		} else {
+			System.out.println("Filme B");
+			System.out.println("Lugares esgotados!");
+			return 0;
+		}
 	}
+
+	private static double filmeC(int lugaresFilmeC) {
+		if (lugaresFilmeC > 0) {
+			System.out.println("Filme C");
+			return 10;
+		} else {
+			System.out.println("Filme C");
+			System.out.println("Lugares esgotados!");
+			return 0;
+		}
+	}
+
+	// Tipo do Ingresso
 }
