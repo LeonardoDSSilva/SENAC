4501d33033e0b5c022261958b5f7b7ca7891cbf9
Leonardo Silva <ldss1@outlook.com>
2022-10-27 20:02:46 -0300

    projeto

---
+++ 1SEM_PI_I/PI_OCaronte/src/caronte/Caminho02.java
@@ -0,0 +1,5 @@
+package caronte;
+
+public class Caminho02 {
+	
+}
---
+++ 1SEM_PI_I/PI_OCaronte/src/caronte/_Principal.java
@@ -0,0 +1,9 @@
+package caronte;
+
+public class _Principal {
+	
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
