7122426a0c0c7b21b1ccc62bdde925b04746a818
Leonardo Silva <ldss1@outlook.com>
2022-10-27 21:05:19 -0300

    projeto

--- 1SEM_PI_I/PI_OCaronte/src/caronte/_Principal.java
+++ 1SEM_PI_I/PI_OCaronte/src/caronte/_Principal.java
@@ -23,16 +23,28 @@ public class _Principal {
 		System.out.println("Você está prestes a embarcar em uma jornada pelo Inferno.");
 
 
-
+		System.out.println("Escolha o caminho que deseja seguir:");
 		System.out.println("1 - Caminho 01");
 		System.out.println("2 - Caminho 02");
 		System.out.println("3 - Caminho 03");
 		System.out.println("4 - Caminho 04");
 		System.out.println("5 - Sair");
-
-		System.out.println("Escolha um caminho: ");
+	
+		System.out.println("O caminho: ");
 		caminho = sc_Num.nextInt();
 
+		menuOCaminho(caminho);
+
+
+
+	}
+	public static void limparTela() {
+		System.out.print("\033[H\033[2J");  
+		System.out.flush();
+	}
+	public static void menuOCaminho(int caminho) {
+		int moedas = 0;
+	
 		switch (caminho) {
 		case 1:
 			Caminho01 caminho01 = new Caminho01();
@@ -55,4 +67,6 @@ public class _Principal {
 			break;
 		}
 	}
-}
+	}
+	
+
