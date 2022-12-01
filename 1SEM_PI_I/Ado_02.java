0d7f9afcd17e1b924d66de584433c18056bee7ba
Leonardo Silva <ldss1@outlook.com>
2022-12-01 14:37:03 -0300

    ADO 02

--- 1SEM_PI_I/Ado_02.java
+++ 1SEM_PI_I/Ado_02.java
@@ -3,31 +3,46 @@ import java.util.Random;
 
 public class Ado_02 {
 	public static void main(String[] args) {
+		limparTela();
 		System.out.println("### Cara ou Coroa ###");
-		int vitorias = 0, contador = 0;
+		int vitorias = 0, contador = 0, computador, usuario;
 		Scanner iScanner = new Scanner(System.in);
 		Random rand = new Random();
+		temporizador();
 
 		do{
-			System.out.print("\033[H\033[2J");
-			System.out.flush();
-
-			System.out.println("O que você escolhe?");
-			System.out.println("  0 - Cara");
-			System.out.println("  1 - Coroa");
+			limparTela();
+			System.out.println( " " + (contador+1) + "ª tentativa de 5");
+			System.out.println("  O que você escolhe?");
+			System.out.println("    0 - Cara");
+			System.out.println("    1 - Coroa");
 			System.out.print("-> ");
+			usuario = iScanner.nextInt();
 
-			int computador = rand.nextInt(2);
-
-			int usuario = iScanner.nextInt();
+			computador = rand.nextInt(2);
 			if (usuario >= 0 && usuario <= 1) {
 				if (usuario == computador){
 					vitorias += 1;
 				}
 				contador += 1;
+				System.out.println("\n  Deu " + (computador == 0 ? "Cara" : "Coroa"));
+				System.out.println((usuario == computador ? "  Você ganhou !!" : "  Não foi dessa vez !"));
+				temporizador();
 			}
 		} while (contador < 5);
-
+		limparTela();
 		System.out.println("Você ganhou " + vitorias + " vezes de 5 jogadas");
+	}
+	public static void temporizador() {
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void limparTela() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 }
