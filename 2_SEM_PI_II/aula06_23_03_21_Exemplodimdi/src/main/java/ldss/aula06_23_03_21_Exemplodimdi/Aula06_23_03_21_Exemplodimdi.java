9fcde697c816c292dd4140c0d488a46e2f1fe4fc
Leonardo Silva <ldss1@outlook.com>
2023-03-21 23:23:50 -0300

    AULA 6 - LAYOUT E VALIDAÇÃO

---
+++ 2_SEM_PI_II/aula06_23_03_21_Exemplodimdi/src/main/java/ldss/aula06_23_03_21_Exemplodimdi/Aula06_23_03_21_Exemplodimdi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.aula06_23_03_21_Exemplodimdi;
+
+/**
+ *
+ * @author ldss1
+ */
+public class Aula06_23_03_21_Exemplodimdi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
