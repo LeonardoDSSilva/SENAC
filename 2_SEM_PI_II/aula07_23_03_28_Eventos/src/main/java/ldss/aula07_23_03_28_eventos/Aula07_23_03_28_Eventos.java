cb75cfc3fb66df2c986d3f0560f7d7e73a69076f
Leonardo Silva <ldss1@outlook.com>
2023-03-28 22:06:13 -0300

    Eventos

---
+++ 2_SEM_PI_II/aula07_23_03_28_Eventos/src/main/java/ldss/aula07_23_03_28_eventos/Aula07_23_03_28_Eventos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.aula07_23_03_28_eventos;
+
+/**
+ *
+ * @author ldss1
+ */
+public class Aula07_23_03_28_Eventos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
