64bb8bd9e3eb42019f43001719a1e30b9ee1e89d
Leonardo Silva <ldss1@outlook.com>
2023-04-11 22:55:29 -0300

    New Project

---
+++ 2_SEM_PI_II/aula08_JDBC/src/main/java/ldss/aula08_jdbc/Aula08_JDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.aula08_jdbc;
+
+/**
+ *
+ * @author ldss1
+ */
+public class Aula08_JDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
