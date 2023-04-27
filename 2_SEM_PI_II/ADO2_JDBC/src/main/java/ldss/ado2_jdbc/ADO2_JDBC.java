8f0005b0521937c2ebf0dcb841ffc5147ea2b5aa
Leonardo Silva <ldss1@outlook.com>
2023-04-27 19:40:03 -0300

    Inclusion of JDC DATE

---
+++ 2_SEM_PI_II/ADO2_JDBC/src/main/java/ldss/ado2_jdbc/ADO2_JDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.ado2_jdbc;
+
+/**
+ *
+ * @author ldss1
+ */
+public class ADO2_JDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
