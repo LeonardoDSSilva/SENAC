75734a9a0bb858f569be240ad0d67bbc63d1db6f
Leonardo Silva <ldss1@outlook.com>
2023-05-01 12:41:16 -0300

    CRUD

--- 2_SEM_PW/Epic Task/assets/js/crud.js
+++ 2_SEM_PW/Epic Task/assets/js/crud.js
@@ -4,30 +4,51 @@ function cadastrar() {
 	const titulo = document.querySelector('#titulo').value
 	const pontos = document.querySelector('#pontos').value
 	const categoria = document.querySelector('#categoria').value
-	console.log('Cadastrando...')
-	console.log(titulo, pontos, categoria)
+
+	// Criar um objeto
+	const tarefa = {
+		titulo,
+		pontos,
+		categoria
+	}
+
+	document.querySelector('#tarefas').innerHTML += criarCard(tarefa)
+
+	// Limpar os campos
+	document.querySelector('#titulo').value = ''
+	document.querySelector('#pontos').value = ''
+	document.querySelector('#categoria').value = ''
+
+	// Fechar a janela modal
+	
 }
 
-function criarCard(){
-	const card = `
-	<div class="card col-lg-3 col-md-6 col-sm-12">
-	<div class="card-header">
-		Estudat o JS
-	</div>
-	<div class="card-body">
-		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-		<p class="card-text">Estudo</p>
-		<span class="badge text-bg-warning">150pts</span>
-	</div>
-	<div class="card-footer">
-		<a href="#" class="btn btn-success" title="marcar como concluida">
-			<i class="bi bi-check2-square"></i>
-		</a>
-		<a href="#" class="btn btn-danger" title="remover tarefa">
-			<i class="bi bi-trash3"></i>
-		</a>
+function apagar(botao){
+    botao.parentNode.parentNode.parentNode.remove()
+}
 
-	</div>
-
-	` // Template literals
+function criarCard(tarefa) {
+    const card = `
+        <div class="col-lg-3 col-md-6 col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                ${tarefa.titulo}
+            </div>
+            <div class="card-body">
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">${tarefa.categoria}</p>
+                <span class="badge text-bg-warning">${tarefa.pontos}pt</span>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-success" title="marcar como concluída">
+                    <i class="bi bi-check2"></i>
+                </a>
+                <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar tarefa">
+                    <i class="bi bi-trash3"></i>
+                </a>
+            </div> <!-- card footer -->
+        </div> <!-- card -->
+    </div> <!-- col -->
+    ` 
+    return card
 }
