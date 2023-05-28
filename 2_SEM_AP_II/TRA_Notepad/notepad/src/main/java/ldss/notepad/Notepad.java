52f9bb0974758e905c569e2f64dbec1d267da538
Leonardo Silva <ldss1@outlook.com>
2023-05-27 23:39:38 -0300

    Project

---
+++ 2_SEM_AP_II/TRA_Notepad/notepad/src/main/java/ldss/notepad/Notepad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.notepad;
+
+/**
+ *
+ * @author ldss1
+ */
+public class Notepad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
