f4b83fc1e5d34a64795104c87d4eb22e26eda8a8
Leonardo Silva <ldss1@outlook.com>
2023-05-29 23:35:38 -0300

    New Version

---
+++ 2_SEM_PW/themarvel/src/pages/index.js
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+      <>
+        <Header/>
+        <main>
+          <div className="h-96"></div>
+          <h2>FILMES</h2>
+          <h2>ANIMES</h2>
+          <h2>DESENHOS</h2>
+          <h2>HEROIS</h2>
+          <h2>SAGAS</h2>
+
+
+        </main>
+      </>
+  )
+}
