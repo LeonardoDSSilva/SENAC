e7fb3e858de4479edeef0e5b883bc6d13e98f00e
Leonardo Silva <ldss1@outlook.com>
2023-06-01 23:54:28 -0300

    Pilha

---
+++ 2_SEM_AP_II/_Aulas/06.01/pilha/src/Pilha.java
@@ -0,0 +1,67 @@
+public class Pilha {
+	private int[] vetorPilha;
+	private boolean isFull = false;
+	private boolean isEmpty = true;
+
+	int top = -1;
+
+	Pilha() {
+		vetorPilha = new int[5];
+	}
+
+	
+	Pilha(int tamanhoPilha) {
+		vetorPilha = new int[tamanhoPilha];
+	}
+
+	public boolean push(int valorEmpilhar) {
+		if(!isFull) {
+			top++;
+			vetorPilha[top] = valorEmpilhar;
+			isEmpty = true;
+
+			if(top == vetorPilha.length - 1) {
+				isFull = true;
+			}
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public int pop() {
+		if(!isEmpty) {
+			int valorRetorno = vetorPilha[top];
+			isFull = false;
+			top--;
+			if(isFull) {
+				isFull = false;
+			}
+			return valorRetorno;
+		}
+		return -1;
+	}
+
+	public int peek() {
+		if(!isEmpty) {
+			return vetorPilha[top];
+		}
+		return -1;
+	}
+
+	public void clear() {
+		top = -1;
+		isEmpty = true;
+		isFull = false;
+	}
+
+	public boolean getIsEmpty() {
+		return isEmpty;
+	}
+
+	public boolean getIsFull() {
+		return isFull;
+	}
+
+
+}
