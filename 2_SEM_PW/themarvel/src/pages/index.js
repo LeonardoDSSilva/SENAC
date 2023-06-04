00690703b13eee51109fff50e4ee35da96adbcda
Leonardo Silva <ldss1@outlook.com>
2023-06-03 23:47:55 -0300

    New Version

--- 2_SEM_PW/themarvel/src/pages/index.js
+++ 2_SEM_PW/themarvel/src/pages/index.js
@@ -1,19 +1,45 @@
 import Header from "@/components/Header";
+import Secao from "@/components/Secao";
+import Titulo from "@/components/Titulo";
 
 export default function Home() {
-  return (
-      <>
-        <Header/>
-        <main>
-          <div className="h-96"></div>
-          <h2>FILMES</h2>
-          <h2>ANIMES</h2>
-          <h2>DESENHOS</h2>
-          <h2>HEROIS</h2>
-          <h2>SAGAS</h2>
+	const url = `${process.env.NEXT_PUBLIC_MARVEL_API_URL}characters?ts=${process.env.NEXT_PUBLIC_MARVEL_API_TS}&apikey=${process.env.NEXT_PUBLIC_MARVEL_API_KEY}&hash=${process.env.NEXT_PUBLIC_MARVEL_API_HASH}&limit=10`;
+	console.log(url);
+
+	fetch(url)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 
-        </main>
-      </>
-  )
+	const filmes = [
+		{
+			titulo: "Super Mario Bros",
+			nota: 5.5,
+			poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
+		},
+		{
+			titulo: "Mae",
+			nota: 5.5,
+			poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jPLNkniLLLsnvqRDncIyFyTcXsN.jpg",
+		}
+	]
+	return (
+		<>
+			<Header />
+			<main>
+				<div className="h-96"></div>
+				<Titulo>FILMES</Titulo>
+				<Secao conteudo={filmes} />
+				<Titulo>ANIMES</Titulo>
+
+				<Titulo>HQs</Titulo>
+				<Titulo>DESENHOS</Titulo>
+				<Titulo>HEROIS</Titulo>
+				<Titulo>SAGAS</Titulo>
+
+
+			</main>
+		</>
+	)
 }
