78137b6f0be8e2d236b03ace3769dcd1ed1dcded
Leonardo Silva <ldss1@outlook.com>
2023-06-04 23:50:26 -0300

    New Version

--- 2_SEM_PW/themarvel/src/pages/index.js
+++ 2_SEM_PW/themarvel/src/pages/index.js
@@ -1,36 +1,23 @@
 import Header from "@/components/Header";
 import Secao from "@/components/Secao";
 import Titulo from "@/components/Titulo";
+import { useState } from "react";
 
 export default function Home() {
+	const [characters, setCharacters] = useState([]);
 	const url = `${process.env.NEXT_PUBLIC_MARVEL_API_URL}characters?ts=${process.env.NEXT_PUBLIC_MARVEL_API_TS}&apikey=${process.env.NEXT_PUBLIC_MARVEL_API_KEY}&hash=${process.env.NEXT_PUBLIC_MARVEL_API_HASH}&limit=10`;
-	console.log(url);
-
 	fetch(url)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => setCharacters(response.data.results))
 	.catch(err => console.error(err));
 
-
-	const filmes = [
-		{
-			titulo: "Super Mario Bros",
-			nota: 5.5,
-			poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-		},
-		{
-			titulo: "Mae",
-			nota: 5.5,
-			poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jPLNkniLLLsnvqRDncIyFyTcXsN.jpg",
-		}
-	]
 	return (
 		<>
 			<Header />
 			<main>
 				<div className="h-96"></div>
 				<Titulo>FILMES</Titulo>
-				<Secao conteudo={filmes} />
+				<Secao conteudo={characters} />
 				<Titulo>ANIMES</Titulo>
 
 				<Titulo>HQs</Titulo>
