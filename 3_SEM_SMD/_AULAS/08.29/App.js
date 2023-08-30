b22b3a2182178fd294623be8fc9e23b22a115705
Leonardo Silva <ldss1@outlook.com>
2023-08-29 22:00:04 -0300

    AULA 08.29

---
+++ 3_SEM_SMD/_AULAS/08.29/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+const App = () => {
+  return(
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>Ola Mundo</Text>
+    </View>
+  )};
+
+export default App;
