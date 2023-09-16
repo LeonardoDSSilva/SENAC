fd3dc8a661f55ea221cbc58c87d70bc585abdb2b
Leonardo Silva <ldss1@outlook.com>
2023-09-16 00:59:43 -0300

    Start ADO02

---
+++ 3_SEM_ED/ADO/ADO02/src/main/java/ldss/ado02/ADO02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ldss.ado02;
+
+/**
+ *
+ * @author ldss1
+ */
+public class ADO02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
