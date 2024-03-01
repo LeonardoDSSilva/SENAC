632565927142b928975149b345db6de681100860
Leonardo Silva <ldss1@outlook.com>
2024-03-01 15:42:56 -0300

    Refactor UsaSingleton to utilize singleton instance directly

--- 4_SEM_AS/AULAS/a_02-29/ExemploSingleton/src/cliente/UsaSingleton.java
+++ 4_SEM_AS/AULAS/a_02-29/ExemploSingleton/src/cliente/UsaSingleton.java
@@ -4,18 +4,10 @@ import singleton.Singleton;
 
 public class UsaSingleton extends Thread{
 
-    @Override
-    public void run(){
-        Singleton s = Singleton.getInstance();
-        System.out.println(s);
-
-    }
-
     public static void main(String[] args) throws Exception {
-        UsaSingleton t1 = new UsaSingleton();
-        UsaSingleton t2 = new UsaSingleton();
-
-        t1.start();
-        t2.start();
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
